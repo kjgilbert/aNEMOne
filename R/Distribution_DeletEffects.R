78cc8ff23f9b1cf3d3656070dfa21cef4a3cd83f
kjgilbert <kgilbert@zoology.ubc.ca>
2017-03-06 10:29:16 +0100

    fix plotting for delet effects

--- R/Distribution_DeletEffects.R
+++ R/Distribution_DeletEffects.R
@@ -24,11 +24,15 @@
 
 
 dist.delet.effects <- function(file, num.loci, xlim.ho=c(0,1), xlim.he=c(0,1)){
-	delet.traits <- read.table(file, header=TRUE, sep=" ", stringsAsFactors=FALSE)
-	# delet loci effect sizes:
-	# because there are 1000 loci, get rid of spot 1, pop ID and last 4 spots - age, sex, ped, origin
-	ho <- delet.traits[1,2:(num.loci+1)]
-	he <- delet.traits[2,2:(num.loci+1)]
+#	delet.traits <- read.table(file, header=TRUE, sep=" ", stringsAsFactors=FALSE)
+  delet.traits <- matrix(scan(file, skip=1, nlines=2, what="numeric()"), ncol=num.loci+5, byrow=TRUE)
+  # strip the -1's from nemo's extra information
+  # because there are 1000 loci, get rid of spot 1, pop ID and last 4 spots - age, sex, ped, origin
+  delet.traits <- delet.traits[, -c(1,(num.loci+2):(num.loci+5))]
+  
+  # delet loci effect sizes:
+	ho <- as.numeric(delet.traits[1,])
+	he <- as.numeric(delet.traits[2,])
 	
 	par(mfrow=c(1,2))
 	hist(as.matrix(ho), col="steelblue1", breaks=50, xlab="Homozygous Effect Size", main="", xlim=xlim.ho)
