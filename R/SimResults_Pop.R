43223f730b907c1b95f5d5449c6ab90e5f31732b
kjgilbert <kgilbert@zoology.ubc.ca>
2017-03-07 15:17:55 +0100

    update to better heatmaps on fitness plots

--- R/SimResults_Pop.R
+++ R/SimResults_Pop.R
@@ -67,6 +67,23 @@ sim.results.pop <- function(input.stats.file, which.rep=NULL, par.size=c(2,2), s
 
 fit.results.landscape <- function(input.fit.file, patches.x, patches.y, fitness.type="total"){
 
+	# custom colors
+	purple=rgb(1,0,1)
+	red=rgb(1,0,0)
+	yellow=rgb(1,1,0)
+	green=rgb(0,1,0)
+	teal=rgb(0,1,1)
+	blue=rgb(0,0,1)
+	white=rgb(1,1,1)
+	whiteToWhite <- colorRampPalette(c(white,white))
+	redToYellow <- colorRampPalette(c(red,yellow))
+	whiteToYellow <- colorRampPalette(c(white, yellow))
+	yellowToTeal <- colorRampPalette(c(yellow, teal))
+	greenToTeal <- colorRampPalette(c(green, teal))
+	tealToBlue <- colorRampPalette(c(teal, blue))
+	yellowToGreen <- colorRampPalette(c(yellow, green))
+
+
 	input <- read.table(input.fit.file, header=TRUE)
 	# column 1 is pop
 	# column 2 is fitness for trait 1 = delet
@@ -82,8 +99,9 @@ fit.results.landscape <- function(input.fit.file, patches.x, patches.y, fitness.
 		# if some patches are empty:
 	if(dim(per.patch.fitness)[1] < total.num.patches){
 		empty.patches <- setdiff(1:total.num.patches, per.patch.fitness$pop)
-		empties <- matrix(0, ncol=5, nrow=length(empty.patches))
-		empty.rows <- data.frame(matrix(cbind(empty.patches, empty.patches, empties), ncol=7, byrow=TRUE))
+		empty.rows <- data.frame(matrix(0, ncol=7, nrow=length(empty.patches)))
+		empty.rows[,1] <- empty.patches
+		empty.rows[,2] <- empty.patches
 		names(empty.rows) <- names(per.patch.fitness)
 		per.patch.fitness <- rbind(per.patch.fitness, empty.rows)
 		per.patch.fitness <- per.patch.fitness[order(per.patch.fitness$pop), ]	
@@ -92,16 +110,16 @@ fit.results.landscape <- function(input.fit.file, patches.x, patches.y, fitness.
 	if(fitness.type == "total" | fitness.type == "all"){
 		# make total fitness into a matrix matched to the landscape
 		total.fit.mat <- matrix(per.patch.fitness$total.fitness, nrow=patches.y, ncol=patches.x, byrow=FALSE)
-		heatmap(x=total.fit.mat, col=heat.colors(256), scale="column", margins=c(2,2), xlab="x axis", ylab="y axis", Rowv=NA, Colv=NA, labRow=NA, labCol=NA, main="Mean total fitness per patch")
+		heatmap.2(x=total.fit.mat, dendrogram='none', labRow=NA, labCol=NA, margins=c(2,1), trace='none', na.color="white", keysize=1, key.ylab=NA, key.title=NA, density.info='none', key.par=list(yaxt="n"), xlab="axis of expansion ->", col=c(whiteToWhite(1), redToYellow(60), yellowToGreen(15), greenToTeal(15), tealToBlue(15)), main="Mean total fitness per patch")
 	}
 	if(fitness.type == "quanti" | fitness.type == "all"){
 		# make quanti fitness into a matrix matched to the landscape
 		quanti.fit.mat <- matrix(per.patch.fitness$trait2, nrow=patches.y, ncol=patches.x, byrow=FALSE)
-		heatmap(x=quanti.fit.mat, col=heat.colors(256), scale="column", margins=c(2,2), xlab="x axis", ylab="y axis", Rowv=NA, Colv=NA, labRow=NA, labCol=NA, main="Mean quanti fitness per patch")
+		heatmap.2(x=quanti.fit.mat, dendrogram='none', labRow=NA, labCol=NA, margins=c(2,1), trace='none', na.color="white", keysize=1, key.ylab=NA, key.title=NA, density.info='none', key.par=list(yaxt="n"), xlab="axis of expansion ->", col=c(whiteToWhite(1), redToYellow(60), yellowToGreen(15), greenToTeal(15), tealToBlue(15)), main="Mean quanti fitness per patch")
 	}
 	if(fitness.type == "delet" | fitness.type == "all"){
 		# make delet fitness into a matrix matched to the landscape
 		delet.fit.mat <- matrix(per.patch.fitness$trait1, nrow=patches.y, ncol=patches.x, byrow=FALSE)
-		heatmap(x=delet.fit.mat, col=heat.colors(256), scale="column", margins=c(2,2), xlab="x axis", ylab="y axis", Rowv=NA, Colv=NA, labRow=NA, labCol=NA, main="Mean delet fitness per patch")
+		heatmap.2(x=delet.fit.mat, dendrogram='none', labRow=NA, labCol=NA, margins=c(2,1), trace='none', na.color="white", keysize=1, key.ylab=NA, key.title=NA, density.info='none', key.par=list(yaxt="n"), xlab="axis of expansion ->", col=c(whiteToWhite(1), redToYellow(60), yellowToGreen(15), greenToTeal(15), tealToBlue(15)), main="Mean delet fitness per patch")
 	}
 }
